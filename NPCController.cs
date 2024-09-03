77c16582bbe0bff7261621fae98e70657c6b45d1
glen <glen.piercev@gmail.com>
2024-09-02 17:22:25 -0700

    slightly more interesting fleeing behavior

--- NPCController.cs
+++ NPCController.cs
@@ -3,13 +3,23 @@ using System;
 
 public partial class NPCController : Node2D {
 	private Node2D player;
+	private float noticeDistance = 100.0f;
+	private float happyDistance = 250.0f;
+	private bool isHappy = true;
 	public override void _Ready() {
 		player = GetParent().GetNode<Node2D>("Player");
 	}
 
 	public override void _Process(double delta) {
 		Vector2 direction = Vector2.Zero;
-		if(Position.DistanceTo(player.Position) < 100) {
+		if (Position.DistanceTo(player.Position) < noticeDistance) {
+			isHappy = false;
+		}
+		if (Position.DistanceTo(player.Position) > happyDistance) {
+			isHappy = true;
+		}
+
+		if(!isHappy) {
 			RunFromPlayer(player, ref direction);
 		}
 	}
